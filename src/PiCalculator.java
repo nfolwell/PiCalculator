2ca3862363cffa3d00f316397d1fb1e910987e30
Nathan Folwell <nfolwell@schools.nyc.gov>
2024-03-14 11:20:46 -0400

    Trying to figur out BigDecimal

--- src/PiCalculator.java
+++ src/PiCalculator.java
@@ -4,13 +4,26 @@ public class PiCalculator
 {
     public static void main(String[] args)
     {
-       double pi = 0.0;
-       for (int k = 0; k < 100; k++)
-       {
-        System.out.println((1.0/Math.pow(16.0,k)*((4.0/(8.0*k+1) - 2.0/(8.0*k+4) - 1.0/(8.0*k+5) - 1.0/(8.0*k+6)))));
-        System.out.print(k+": ");
-        System.out.println(pi);
-       }
+       BigDecimal pi = new BigDecimal(0.0);
+       BigDecimal one = new BigDecimal(1.0);
+       BigDecimal sixteen = new BigDecimal(16.0);
+       BigDecimal four = new BigDecimal(4.0);
+       BigDecimal two = new BigDecimal(2.0);
+       two.setScale(100);
+       BigDecimal three = new BigDecimal(3.0);
+       three.setScale(100);
+       BigDecimal twoThirds = new BigDecimal(1.0);
+       twoThirds = two.divide(three);
+       System.out.println(twoThirds);
+       
+
+       //for (int k = 0; k < 100; k++)
+       //{
+        //System.out.print(k+": ");
+        //System.out.println((1.0/Math.pow(16.0,k)*((4.0/(8.0*k+1) - 2.0/(8.0*k+4) - 1.0/(8.0*k+5) - 1.0/(8.0*k+6)))));
+        //System.out.print(k+": ");
+       // System.out.println(pi);
+       //}
 
     }
 }
