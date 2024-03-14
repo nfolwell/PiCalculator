5b304f7e71e65146faed56f3acadeb7169d1c4c8
Nathan Folwell <nfolwell@schools.nyc.gov>
2024-03-14 16:29:13 -0400

    Sending to github

--- src/PiCalculator.java
+++ src/PiCalculator.java
@@ -4,19 +4,31 @@ public class PiCalculator
 {
     public static void main(String[] args)
     {
+       MathContext mc = new MathContext(20,RoundingMode.HALF_UP);
        BigDecimal pi = new BigDecimal(0.0);
        BigDecimal one = new BigDecimal(1.0);
        BigDecimal sixteen = new BigDecimal(16.0);
+       BigDecimal eight = new BigDecimal(8.0);
        BigDecimal four = new BigDecimal(4.0);
        BigDecimal two = new BigDecimal(2.0);
-       two.setScale(100);
-       BigDecimal three = new BigDecimal(3.0);
-       three.setScale(100);
-       BigDecimal twoThirds = new BigDecimal(1.0);
-       twoThirds = two.divide(three);
-       System.out.println(twoThirds);
        
+       //BigDecimal twoThirds = new BigDecimal(1.0);
+       //a = 4/(8*(k+1)) 
+       //int k = 1;
+       for (int k = 0; k < 10; k++)
+       {
+        BigDecimal a = four.divide(eight.multiply(new BigDecimal(k + 1),mc),mc);
+        //b = 2.0/(8.0*k+4)
+        BigDecimal b = two.divide(eight.multiply(new BigDecimal(k+4),mc),mc);
+        //c = 1.0(8.0*(k+5));
+        BigDecimal c = one.divide(eight.multiply(new BigDecimal(k + 5, mc),mc),mc);
+        //d 1.0/(8.0*(k+6))
+        BigDecimal d = one.divide(eight.multiply(new BigDecimal(k+6,mc),mc),mc);
 
+        pi = pi.add((one.divide(sixteen,mc)).multiply(a.subtract(b,mc).subtract(c,mc).subtract(d,mc),mc),mc);
+        System.out.println(pi);
+       }
+       
        //for (int k = 0; k < 100; k++)
        //{
         //System.out.print(k+": ");
