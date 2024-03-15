e3e32da154d873e7fb3bf9f9218dbcea0da56736
Nathan Folwell <nfolwell@schools.nyc.gov>
2024-03-15 09:24:42 -0400

    Trying to get the sequence together. Finally got it!

--- src/PiCalculator.java
+++ src/PiCalculator.java
@@ -4,7 +4,8 @@ public class PiCalculator
 {
     public static void main(String[] args)
     {
-       MathContext mc = new MathContext(20,RoundingMode.HALF_UP);
+       int digits = 3000;
+       MathContext mc = new MathContext(digits,RoundingMode.HALF_UP);
        BigDecimal pi = new BigDecimal(0.0);
        BigDecimal one = new BigDecimal(1.0);
        BigDecimal sixteen = new BigDecimal(16.0);
@@ -12,30 +13,29 @@ public class PiCalculator
        BigDecimal four = new BigDecimal(4.0);
        BigDecimal two = new BigDecimal(2.0);
        
-       //BigDecimal twoThirds = new BigDecimal(1.0);
-       //a = 4/(8*(k+1)) 
-       //int k = 1;
+       for (int k = 0; k < digits; k++)
+       {
+        //a = 4/(8*(k+1)) 
+        BigDecimal a = four.divide(eight.multiply(new BigDecimal(k,mc)).add(new BigDecimal(1, mc),mc),mc);
+        //b = 2.0/(8.0*k+4)
+        BigDecimal b = two.divide(eight.multiply(new BigDecimal(k,mc)).add(new BigDecimal(4, mc),mc),mc);
+        //c = 1.0(8.0*(k+5));
+        BigDecimal c = one.divide(eight.multiply(new BigDecimal(k,mc)).add(new BigDecimal(5, mc),mc),mc);
+        //d 1.0/(8.0*(k+6))
+        BigDecimal d = one.divide(eight.multiply(new BigDecimal(k,mc)).add(new BigDecimal(6, mc),mc),mc);
+
+        pi = pi.add((one.divide(sixteen,mc).pow(k,mc)).multiply((a.subtract(b,mc)).subtract(c,mc).subtract(d,mc),mc),mc);
+       
+       }
+       System.out.println(pi);
+     /*  double dPi = 0.0;
        for (int k = 0; k < 10; k++)
        {
-        BigDecimal a = four.divide(eight.multiply(new BigDecimal(k + 1),mc),mc);
-        //b = 2.0/(8.0*k+4)
-        BigDecimal b = two.divide(eight.multiply(new BigDecimal(k+4),mc),mc);
-        //c = 1.0(8.0*(k+5));
-        BigDecimal c = one.divide(eight.multiply(new BigDecimal(k + 5, mc),mc),mc);
-        //d 1.0/(8.0*(k+6))
-        BigDecimal d = one.divide(eight.multiply(new BigDecimal(k+6,mc),mc),mc);
-
-        pi = pi.add((one.divide(sixteen,mc)).multiply(a.subtract(b,mc).subtract(c,mc).subtract(d,mc),mc),mc);
-        System.out.println(pi);
-       }
-       
-       //for (int k = 0; k < 100; k++)
-       //{
+        System.out.print(k+": ");
+        dPi += (1.0/Math.pow(16.0,k)*((4.0/(8.0*k+1) - 2.0/(8.0*k+4) - 1.0/(8.0*k+5) - 1.0/(8.0*k+6))));
         //System.out.print(k+": ");
-        //System.out.println((1.0/Math.pow(16.0,k)*((4.0/(8.0*k+1) - 2.0/(8.0*k+4) - 1.0/(8.0*k+5) - 1.0/(8.0*k+6)))));
-        //System.out.print(k+": ");
-       // System.out.println(pi);
-       //}
+        System.out.println(dPi);
+       }*/
 
     }
 }
